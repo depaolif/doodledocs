9a08531fdecc2f0f0fd6ab3d76cc032637e25189
nanyr <nanyramirez0@gmail.com>
2017-04-17 16:44:05 -0400

    generated models and controllers and routes

---
+++ doodledocs-rails/app/controllers/v1/accounts_controller.rb
@@ -0,0 +1,2 @@
+class V1::AccountsController < ApplicationController
+end
--- doodledocs-rails/config/routes.rb
+++ doodledocs-rails/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    namespace :v1 do
+      resources :accounts, only: [:create] do
+        resources :images, only: [:create, :update, :destroy]
+      end
+
+      resources :sessions, only: [:create, :destroy]
+    end
+
 end
