6cc9676fb14e4daf45fc529e123c5e308f947892
Noah Schechter <noahschechter7@gmail.com>
2017-04-17 15:26:13 -0400

    got drawing to work

--- doodledocs-react/src/App.js
+++ doodledocs-react/src/App.js
@@ -7,8 +7,10 @@ class App extends Component {
       super()
 
       this.canvas = null
+      this.context = null
       this.getMousePos = this.getMousePos.bind(this)
       this.writeMessage = this.writeMessage.bind(this)
+      this.isPainting = false
   }
 
   componentWillMount() {
@@ -16,14 +18,32 @@ class App extends Component {
 
   componentDidMount() {
     this.canvas = document.getElementById('app-canvas');
+    this.context = this.canvas.getContext('2d')
+    this.canvas.addEventListener('mousedown', (event) => {
+      let mousePos = this.getMousePos(this.canvas, event)
+      this.context.beginPath()
+      this.context.moveTo(mousePos.x, mousePos.y)
+      this.isPainting = true
+    }, false)
     this.canvas.addEventListener('mousemove', (event) => {
-      let mousePos = this.getMousePos(this.canvas, event);
-      let message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-      let context = this.canvas.getContext('2d')
-      context.strokeText('YOOOO', mousePos.x, mousePos.y)
-      this.writeMessage(this.canvas, message);
-    }, false);
-    let ctx = this.canvas.getContext('2d')
+      if (this.isPainting) {
+        let mousePos = this.getMousePos(this.canvas, event)
+        this.context.lineTo(mousePos.x, mousePos.y)
+        this.context.stroke()
+      }
+    })
+    this.canvas.addEventListener('mouseup', (event) => {
+      this.isPainting = false
+      //this.context.clearRect(0, 0, 1000, 1000)
+    }, false)
+    // this.canvas.addEventListener('mousemove', (event) => {
+    //   let mousePos = this.getMousePos(this.canvas, event);
+    //   let message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    //   let context = this.canvas.getContext('2d')
+    //   //context.strokeText('YOOOO', mousePos.x, mousePos.y)
+    //   this.writeMessage(this.canvas, message);
+    // }, false);
+    // let ctx = this.canvas.getContext('2d')
   }
 
   writeMessage(canvas, message) {
