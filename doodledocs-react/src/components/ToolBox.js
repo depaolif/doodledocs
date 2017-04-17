48e33af98fc399287a4048d8a43c229373126743
Noah Schechter <noahschechter7@gmail.com>
2017-04-17 16:20:17 -0400

    adding toolbox

---
+++ doodledocs-react/src/components/ToolBox.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+class ToolBox extends Component {
+	constructor() {
+		super()
+
+		this.state = {
+			focusedItem: null
+		}
+	}
+
+	render() {
+		return (
+			<div className="toolbox">
+				 
+			</div>
+		)
+	}
+}
+
+export default ToolBox
