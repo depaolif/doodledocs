ce57ad18fd30017683ab23dd68a78cc0a22281ec
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-04-18 14:06:51 -0400

    added profile view

--- doodledocs-react/src/components/Profile.js
+++ doodledocs-react/src/components/Profile.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import axios from 'axios'
+import ImageItem from './ImageItem'
 
 class Profile extends Component {
+
 	render() {
+		const images = this.props.imageList.map((el, i) => {
+			return <ImageItem key={i} imageId={el.id} imageTitle={el.title} />
+		})
 		return (
 			<div className="profile">
-
+					<h1>{this.props.username}</h1>
+					<ul>
+						{images}
+					</ul>
 			</div>
 		)
 	}
 }
 
-export default Profile
+const mapStateToProps = (state) => ({
+	username: state.account.username,
+	imageList: state.imageList
+})
+
+export default const ConnectedProfile = connect(mapStateToProps)(Profile)
