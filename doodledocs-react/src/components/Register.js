8fbae9f6b0f472701997f046157d88187216d7c3
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-04-24 12:01:31 -0400

    fixed login, register, and toolbox styling

--- doodledocs-react/src/components/Register.js
+++ doodledocs-react/src/components/Register.js
@@ -81,20 +81,20 @@ class Register extends Component {
 
 	render() {
 		const failMessage = this.state.failed ?
-			<div id="failedLogin">
+			<div className="failed-message">
 				<p>Someone with that username already exists. Please try a different username.</p>
 			</div> :
 			null
-		const usernameValid = this.state.usernameValidation ? null : <div>Your username must be more than 3 characters and contain only alphanumeric characters.</div>
-		const passwordValid = this.state.passwordValidation ? null : <div>Your password must be more than 7 characters.</div>
+		const usernameValid = this.state.usernameValidation ? null : <div className="failed-message">Your username must be more than 3 characters and contain only alphanumeric characters.</div>
+	const passwordValid = this.state.passwordValidation ? null : <div className="failed-message">Your password must be more than 7 characters.</div>
 		const isDisabled = this.state.usernameValidation === true && this.state.passwordValidation === true ? null : 'disabled'
 		return (
-			<form onSubmit={this.handleSubmit} className="login_signup" >
+			<form onSubmit={this.handleSubmit} className="login-signup" >
 				{failMessage}
-				<label>Username: </label><input type="text" name="username" value={this.state.username} onChange={this.handleInput} />
+				<label>Username:</label><input type="text" name="username" value={this.state.username} onChange={this.handleInput} />
 				<br></br>
 				{usernameValid}
-				<label>Password: </label><input type="password" name="password" value={this.state.password} onChange={this.handleInput} />
+				<label>Password:</label><input type="password" name="password" value={this.state.password} onChange={this.handleInput} />
 				{passwordValid}
 				<br></br>
 				<input type="submit" value="Register" id="button" disabled={isDisabled}/>
