56ed934762e45fc28ff8b6b96a677591dcbf28c8
nanyr <nanyramirez0@gmail.com>
2017-04-24 16:06:43 -0400

    addded icons for toolbox

--- doodledocs-react/src/components/ToolBox.js
+++ doodledocs-react/src/components/ToolBox.js
@@ -109,26 +109,31 @@ class ToolBox extends Component {
 					<option value='50'>50</option>
 					<option value='100'>100</option>
 				</select>
-				: 
+				:
 					<input type="number" value={this.props.doodle.fontSize} onChange={this.handleChange} />
 				)
 		return (
 			<div className="toolbox">
 					<ColorPicker onChangeComplete={this.handleChangeComplete}/>
-				{this.props.doodle.tool !== 'text' ? <label>Line Width:</label> : <label>Font Size</label>}
+				{this.props.doodle.tool !== 'text' ? <img src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_25/v1493061658/ic_line_weight_black_24dp_1x_a1dquz.png" alt="line width" className="icon" /> : <label>Font Size</label>}
 				{selector}
-				<img name="free" src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_32/v1493059354/ic_edit_black_24dp_1x_ashvkr.png" alt="free" className="icon" onClick={this.handleClick} />
-
-				<button name="line" onClick={this.handleClick}>Line</button>
-				<button name="circle" onClick={this.handleClick}>Circle</button>
-				<img name="rectangle" src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_32/v1493058803/ic_crop_din_black_24dp_1x_tmmokd.png" alt="square" className="icon" onClick={this.handleClick} />
-				<button name="text" onClick={this.handleClick}>Text</button>
-				<button name="webcam-show" onClick={this.handleWebCam}>Webcam</button>
+				<img name="free" src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_25/v1493061803/ic_gesture_black_24dp_1x_ui9sy3.png" alt="free" className="icon" onClick={this.handleClick} />
+				<img name="line"
+				src="http://res.cloudinary.com/dletp3dah/image/upload/a_90/v1493062257/ic_remove_black_24dp_1x_m1b9yy.png" alt="line" className="icon" onClick={this.handleClick}
+				/>
+				<img name="circle"
+				src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_25/v1493060454/ic_radio_button_unchecked_black_24dp_1x_hekiro.png" alt="circle" className="icon" onClick={this.handleClick}
+				/>
+				<img name="rectangle" src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_25/v1493058803/ic_crop_din_black_24dp_1x_tmmokd.png" alt="square" className="icon" onClick={this.handleClick} />
+				<img name="text"
+				src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_25/v1493061813/ic_text_format_black_24dp_1x_jdkbyd.png" alt="text" className="icon" onClick={this.handleClick}/>
+				<img name="webcam-show"
+				 src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_25/v1493063205/ic_photo_camera_black_24dp_1x_yy0ca0.png" alt="webcam" className="icon" onClick={this.handleWebCam} />
 				{this.state.showWebcam ? <video type='hidden' name="webcam-video" id="webcam-video" /> : false}
 				{this.state.showWebcam ? <canvas type='hidden' id='webcam-canvas' /> : false}
 				{this.state.showWebcam ? <button name="webcam" onClick={this.handleWebCamSave}>Take Picture</button> : false}
-				<label>Upload Image:</label>
-				<input onChange={this.handleImageUpload} type="file" name="image" />
+				<img src="http://res.cloudinary.com/dletp3dah/image/upload/c_scale,w_25/v1493063788/ic_attach_file_black_24dp_1x_fsd4du.png" alt="upload_image" className="icon"/>
+				<input onChange={this.handleImageUpload} type="file" name="image"  />
 			</div>
 		)
 	}
